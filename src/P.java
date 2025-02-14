126e4d7f4b693f24f1a2289f166eae0c8c27ee89
AlvaroEscribanoGomez <alvaro.escribanogomez@educa.madrid.org>
2025-02-14 10:47:28 +0100

    Moficiado

---
+++ src/P.java
@@ -0,0 +1,33 @@
+public class P {
+
+    private String n;
+    private double p;
+    private double i;
+
+    // Constructor que inicializa los valores del Producto
+    public P(String n, double p, double i) {
+        this.n = n;
+        this.p = p;
+        this.i = i;
+    }
+
+    // Calcula
+    public double x() {
+        double y = z(p, i);
+        return p + y;
+    }
+
+    // Método privado para calcular impuestos
+    private double z(double p, double i) {
+        return p * (i / 100);
+    }
+
+    // Muestra detalles
+    public void d() {
+        System.out.println("Producto: " + n);
+        System.out.println("Precio base: " + p);
+        System.out.println("Porcentaje de impuestos: " + i + "%");
+        System.out.println("Precio final: " + x());
+    }
+
+}
