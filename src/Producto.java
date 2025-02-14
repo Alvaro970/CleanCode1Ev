1341d5978f70230d448c9d51f96256f49556b424
AlvaroEscribanoGomez <alvaro.escribanogomez@educa.madrid.org>
2025-02-14 10:54:12 +0100

    Cambio de nombre(Metodos)

--- src/Producto.java
+++ src/Producto.java
@@ -1,33 +1,35 @@
 public class Producto {
 
-    private String n;
-    private double p;
-    private double i;
+    private String nombreProducto;
+    private double precioBase;
+    private double porcentajeImpuesto;
 
     // Constructor que inicializa los valores del Producto
-    public Producto(String n, double p, double i) {
-        this.n = n;
-        this.p = p;
-        this.i = i;
+    public Producto(String nombreProducto, double precioBase, double porcentajeImpuesto) {
+        this.nombreProducto = nombreProducto;
+        this.precioBase = precioBase;
+        this.porcentajeImpuesto = precioBase;
     }
 
-    // Calcula
-    public double x() {
-        double y = z(p, i);
-        return p + y;
+    // Método para calcular el precio final después de aplicar impuestos
+    public double calcularPrecioFinal() {
+        double impuestosAplicados = calcularImpuestos(precioBase, porcentajeImpuesto);
+        return precioBase + impuestosAplicados;
     }
 
     // Método privado para calcular impuestos
-    private double z(double p, double i) {
-        return p * (i / 100);
+    private double calcularImpuestos(double precioBase, double porcentajeImpuesto) {
+        return precioBase * (porcentajeImpuesto / 100);
     }
 
     // Muestra detalles
-    public void d() {
-        System.out.println("Producto: " + n);
-        System.out.println("Precio base: " + p);
-        System.out.println("Porcentaje de impuestos: " + i + "%");
-        System.out.println("Precio final: " + x());
+    // Método para mostrar los detalles del producto, incluyendo precio final
+    public void mostrarDetallesProducto() {
+        System.out.println("Producto: " + nombreProducto);
+        System.out.println("Precio base: " + precioBase);
+        System.out.println("Porcentaje de impuestos: " + porcentajeImpuesto + "%");
+        System.out.println("Precio final: " + calcularPrecioFinal());
     }
+
 
 }
